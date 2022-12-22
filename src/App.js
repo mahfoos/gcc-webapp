ebb2ac652a19eb041a1672889f85eb9dc09d77ba
Fayas Akram <fayas@cinergix.com>
2022-12-22 20:19:06 +0530

    Google Analytics added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import MainLayOut from "./pages/MainLayOut";
 import RequestPickUp from "./pages/RequestPickUp";
 
 import TrackOrder from "./pages/TrackOrder";
+import useGaTracker from './useGaTracker'
+import TagManager from 'react-gtm-module';
+import { useEffect } from "react";
 
 const App = () => {
+  useGaTracker();
+  // useEffect(() => {
+  //   TagManager.initialize({ gtmId: process.env.REACT_APP_GA});
+  //   }, []);
   return (
     <React.Fragment>
       <Routes>
-        <Route path="/" element={<MainLayOut />}>
+        <Route path="/" element={<MainLayOut />} exact component={ MainLayOut }>
           <Route index element={<Home />} />
           <Route path="about-us" element={<AboutUs />} />
           <Route path="request-pickup" element={<RequestPickUp />} />
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
